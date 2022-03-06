8042123c4d8ef2e615f61e898b3436f11fe281a6
Saurabh Gupta <saurabhguptaviet@gmail.com>
2022-03-06 23:52:47 +0530

    fix:UI changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import axios from "axios";
 import { get } from "lodash";
 import { withRouter } from "react-router-dom";
+
 import Footer from "./Footer";
 
 function Login(props) {
@@ -42,58 +41,43 @@ function Login(props) {
         />
         <h3 className="card-title">Login to Docsumo</h3>
         <div className="card-center">
-          <Form action="" onSubmit={handleSubmit}>
-            <div className="field">
+          <form action="" onSubmit={handleSubmit}>
+            <div className="form-floating mb-3">
               <input
                 type="email"
-                name="email"
+                className="form-control input-field rounded-0"
+                id="floatingInput"
+                placeholder="name@example.com"
                 onChange={(e) => setEmail(e.target.value)}
-                className="input-field"
+                name="email"
               />
-              <label htmlFor="email" className="form-label">
+              <label for="floatingInput" className="form-label">
                 Work Email
               </label>
             </div>
-            <div className="field">
+            <div className="form-floating">
               <input
                 type="password"
                 name="password"
-                id="password"
+                className="form-control input-field rounded-0"
+                id="floatingPassword"
+                placeholder="Password"
                 onChange={(e) => setPassword(e.target.value)}
-                className="input-field"
               />
-              <label htmlFor="password" className="form-label">
+              <label for="floatingPassword" className="form-label">
                 Password
               </label>
             </div>
-            <div class="form-floating mb-3">
-              <input
-                type="email"
-                class="form-control"
-                id="floatingInput"
-                placeholder="name@example.com"
-              />
-              <label for="floatingInput">Email address</label>
-            </div>
-            <div class="form-floating">
-              <input
-                type="password"
-                class="form-control"
-                id="floatingPassword"
-                placeholder="Password"
-              />
-              <label for="floatingPassword">Password</label>
-            </div>
             <div style={{ color: "red" }}> {errorMessage && errorMessage}</div>
-            <Button
+            <button
               block="true"
               size="lg"
-              className="btn-primary"
+              className="btn-primary "
               type="submit"
             >
               Login
-            </Button>
-          </Form>
+            </button>
+          </form>
           <Footer />
         </div>
       </div>
