7497b2c0e9244a92a79637a12e4da9b815536a50
Saurabh Gupta <saurabhguptaviet@gmail.com>
2022-03-07 00:08:17 +0530

    fix:UI changes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,10 +2,31 @@ import React from "react";
 
 function Dashboard({ userData }) {
   return (
-    <div>
-      Dashboard
-      <h2>{userData && userData.user && userData.user.full_name}</h2>
-    </div>
+    <>
+      {userData?.user ? (
+        <div>
+          <h2 className="h4">Welcome to DOCSUMO Dashboard!</h2>
+          <p className="h3">
+            Hello{" "}
+            <span className="text-primary">
+              {userData && userData.user && userData.user.full_name}
+            </span>
+          </p>
+        </div>
+      ) : (
+        <>
+          <p className="h4">You are not logged in.</p>
+          <p className="h3">
+            Please
+            <a href="/" className="text-primary">
+              {" "}
+              Log In{" "}
+            </a>
+            first!
+          </p>
+        </>
+      )}
+    </>
   );
 }
 
